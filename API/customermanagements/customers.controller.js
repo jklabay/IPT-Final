fd9bb79ca7271221675cdeee4688e4ca46fac287
Rolison Makilan D <110706416+Kishiiro@users.noreply.github.com>
2023-05-11 20:37:22 +0800

    Update customers.controller.js

--- API/customermanagements/customers.controller.js
+++ API/customermanagements/customers.controller.js
@@ -1,13 +1,17 @@
-﻿const express = require("express");
+const express = require("express");
 const router = express.Router();
 const Joi = require("joi");
 const validateRequest = require("_middleware/validate-request");
-const customerService = require("./customers.service");
+const Role = require("_helpers/role");
+const employeeService = require("./employee.service");
 const authorize = require("_middleware/authorize");
 
 // routes
-router.post("/create", createSchema, create);
-router.get("/", authorize, getAll);    
+
+router.post("/authenticate", authenticateSchema, authenticate);
+router.post("/register", createSchema, create);
+router.post("/logout", logout);
+router.get("/", authorize, getAll);
 router.get("/:id", authorize, getById);
 router.put("/:id", authorize, updateSchema, update);
 router.delete("/:id", authorize, _delete);
@@ -16,73 +20,104 @@ module.exports = router;
 
 // route functions
 
+function authenticateSchema(req, res, next) {
+  const schema = Joi.object({
+    email: Joi.string().required(),
+    password: Joi.string().required(),
+  });
+  validateRequest(req, next, schema);
+}
+
+function authenticate(req, res, next) {
+  employeeService
+    .authenticate(req.body)
+    .then((employee) => {
+      //send the cookie to the browser
+      res.cookie("token", employee.token);
+      res.json(employee);
+    })
+    .catch(next);
+}
+
+function logout(req, res, next) {
+  try {
+    res.cookie("token", null, {
+      expires: new Date(Date.now()),
+      httpOnly: true,
+    });
+
+    res.status(200).json({ message: "Logout Success" });
+  } catch (e) {
+    next();
+  }
+}
+
 function getAll(req, res, next) {
-    customerService.getAll()
-        .then(users => res.json(users))
-        .catch(next);
+  console.log('test')
+  employeeService
+    .getAll()
+    .then((employees) => res.json(employees))
+    .catch(next);
 }
 
 function getById(req, res, next) {
-    customerService.getById(req.params.id)
-        .then(user => res.json(user))
-        .catch(next);
+  employeeService
+    .getById(req.params.id)
+    .then((user) => res.json(user))
+    .catch(next);
 }
 
 function create(req, res, next) {
-    customerService.create(req.body)
-        .then(() => res.json({ message: 'Customer Details Has been created' }))
-        .catch(next);
+  employeeService
+    .create(req.body)
+    .then(() => res.json({ message: "Employee created" }))
+    .catch(next);
 }
 
 function update(req, res, next) {
-    customerService.update(req.params.id, req.body)
-        .then(() => res.json({ message: 'Customer Details Has been updated' }))
-        .catch(next);
+  employeeService
+    .update(req.params.id, req.body)
+    .then(() => res.json({ message: "Employee updated" }))
+    .catch(next);
 }
 
 function _delete(req, res, next) {
-    customerService.delete(req.params.id)
-        .then(() => res.json({ message: 'Customer Details Has been deleted' }))
-        .catch(next);
+  employeeService
+    .delete(req.params.id)
+    .then(() => res.json({ message: "Employee deleted" }))
+    .catch(next);
 }
 
 // schema functions
 
 function createSchema(req, res, next) {
-    const schema = Joi.object({
-        customerNumber: Joi.string().required(),
-        customerName: Joi.string().required(),
-        contactLastName: Joi.string().required(),
-        contactFirstName: Joi.string().required(),
-        phone: Joi.string().required(),
-        addressLine1: Joi.string().required(),
-        addressLine2: Joi.string().allow(null).required(),
-        city: Joi.string().required(),
-        state: Joi.string().allow(null).required(),  
-        postalCode: Joi.string().allow(null).required(),  
-        country: Joi.string().required(),  
-        salesRepEmployeeNumber: Joi.string().allow(null).required(),  
-        creditLimit: Joi.string().allow(null).required(),  
-      });
-      validateRequest(req, next, schema);
-    
+  const schema = Joi.object({
+    employeeNumber: Joi.number().required(),
+    lastName: Joi.string().required(),
+    firstName: Joi.string().required(),
+    extension: Joi.string().required(),
+    email: Joi.string().email().required(),
+    officeCode: Joi.string().required(),
+    reportsTo: Joi.number().allow(null).required(),
+    jobTitle: Joi.string().valid(Role.Admin, Role.VPSales, Role.President, Role.SalesManager, Role.VPMarketing).required(),
+    password: Joi.string().min(6).required(),
+    confirmPassword: Joi.string().valid(Joi.ref("password")).required(),
+  });
+  validateRequest(req, next, schema);
 }
 
 function updateSchema(req, res, next) {
-    const schema = Joi.object({
-        customerNumber: Joi.string().empty(""),
-        customerName: Joi.string().empty(""),
-        contactLastName: Joi.string().empty(""),
-        contactFirstName: Joi.string().empty(""),
-        phone: Joi.string().empty(""),
-        addressLine1: Joi.string().empty(""),
-        addressLine2: Joi.string().allow(null).empty(""),
-        city: Joi.string().empty(""),
-        state: Joi.string().allow(null).empty(""),  
-        postalCode: Joi.string().allow(null).empty(""),  
-        country: Joi.string().empty(""),  
-        salesRepEmployeeNumber: Joi.string().allow(null).empty(""),  
-        creditLimit: Joi.string().allow(null).empty(""),  
-      });
-    validateRequest(req, next, schema);
+  const schema = Joi.object({
+    employeeNumber: Joi.number().empty(""),
+    lastName: Joi.string().empty(""),
+    firstName: Joi.string().empty(""),
+    extension: Joi.string().empty(""),
+    email: Joi.string().email().empty(""),
+    officeCode: Joi.string().empty(""),
+    reportsTo: Joi.number().allow(null).empty(""),
+    jobTitle: Joi.string().valid(Role.Admin, Role.VPSales, Role.President, Role.SalesManager, Role.VPMarketing).empty(""),
+    password: Joi.string().min(6).empty(""),
+    confirmPassword: Joi.string().valid(Joi.ref("password")).empty(""),
+  }).with("password", "confirmPassword");
+  validateRequest(req, next, schema);
 }
