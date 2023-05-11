5c492402dc18cf308a3407b092a6d0d5ea377719
Rolison Makilan D <110706416+Kishiiro@users.noreply.github.com>
2023-05-11 20:40:06 +0800

    Update customers.model.js

--- API/customermanagements/customers.model.js
+++ API/customermanagements/customers.model.js
@@ -1,87 +1,27 @@
-const bcrypt = require("bcryptjs");
-const db = require("_helpers/db");
-const jwt = require("jsonwebtoken");
-const config = require("config.json");
+const { DataTypes } = require('sequelize');
 
-module.exports = {
-  authenticate,
-  getAll,
-  getById,
-  create,
-  update,
-  delete: _delete,
-};
+module.exports = model;
 
-async function authenticate({ email, password }) {
-  const employee = await db.Employee.scope("withHash").findOne({ where: { email } });
+function model(sequelize) {
+    const attributes = {
+        customerNumber: { type: DataTypes.STRING, allowNull: false , primaryKey: true},
+        customerName: { type: DataTypes.STRING, allowNull: false },
+        contactLastName: { type: DataTypes.STRING, allowNull: false },
+        contactFirstName: { type: DataTypes.STRING, allowNull: false },
+        phone : { type: DataTypes.STRING, allowNull: false },
+        addressLine1 : { type: DataTypes.STRING, allowNull: false },
+        addressLine2: { type: DataTypes.STRING, allowNull: true },
+        city: { type: DataTypes.STRING, allowNull: false },
+        state: { type: DataTypes.STRING, allowNull: true },
+        postalCode: { type: DataTypes.STRING, allowNull: true },
+        country: { type: DataTypes.STRING, allowNull: false },
+        salesRepEmployeeNumber: { type: DataTypes.INTEGER, allowNull: true, foreignKey: true},
+        creditLimit: { type: DataTypes.STRING, allowNull: true }
+    };
 
-  if (!employee || !(await bcrypt.compare(password, employee.passwordHash)))
-    throw "Username or password is incorrect";
+    const options = {
+        timestamps: false
+      };
 
-  // authentication successful
-  const token = jwt.sign({ sub: employee.id }, config.secret, { expiresIn: "7d" });
-  return { ...omitHash(employee.get()), token };
-}
-
-async function getAll() {
-  console.log('execute')
-  return await db.Employee.findAll();
-}
-
-async function getById(id) {
-  return await getEmployee(id);
-}
-
-async function create(params) {
-  // validate
-  if (await db.Employee.findOne({ where: { email: params.email } })) {
-    throw 'Email "' + params.email + '" is already registered';
-  }
-
-  const employee = new db.Employee(params);
-
-  // save employee
-  await employee.save();
-}
-
-async function update(id, params) {
-  const employee = await getEmployee(id);
-
-  // validate
-  const emailChanged = params.email && employee.email !== params.email;
-  if (
-    emailChanged &&
-    (await db.Employee.findOne({ where: { email: params.email } }))
-  ) {
-    throw 'Email "' + params.email + '" is already registered';
-  }
-
-  // hash password if it was entered
-  if (params.password) {
-    params.passwordHash = await bcrypt.hash(params.password, 10);
-  }
-
-  // copy params to employee and save
-  Object.assign(employee, params);
-  await employee.save();
-
-  return omitHash(employee.get());
-}
-
-async function _delete(id) {
-  const employee = await getEmployee(id);
-  await employee.destroy();
-}
-
-// helper functions
-
-async function getEmployee(id) {
-  const employee = await db.Employee.findByPk(id);
-  if (!employee) throw "Employee not found";
-  return employee;
-}
-
-function omitHash(employee) {
-  const { passwordHash, ...userWithoutHash } = employee;
-  return userWithoutHash;
+    return sequelize.define("Customer", attributes, options);
 }
