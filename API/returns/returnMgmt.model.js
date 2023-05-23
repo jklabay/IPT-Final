caca50bebb7cc4482f8a0bac59154d94f135bce0
ralphcanillo <ralp.canillo@gmail.com>
2023-05-23 11:44:01 +0800

    modifying the delete function in return mgmt module

--- API/returns/returnMgmt.model.js
+++ API/returns/returnMgmt.model.js
@@ -8,8 +8,9 @@ function model(sequelize) {
     productCode: { type: DataTypes.STRING, allowNull: false },
     officeCode: { type: DataTypes.STRING, allowNull: false },
     returnedQuantity: { type: DataTypes.INTEGER, allowNull: false },
-    returnDate: { type: DataTypes.DATE, allowNull: false },
-    reason: { type: DataTypes.STRING, allowNull: false }
+    returnDate: { type: DataTypes.DATE, defaultValue: DataTypes.NOW, allowNull: false },
+    reason: { type: DataTypes.TEXT, allowNull: false },
+    isActive: { type: DataTypes.CHAR, defaultValue: '1', allowNull: false }
   };
 
   const options = {
