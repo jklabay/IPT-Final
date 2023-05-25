b1673736dc23a8fd93fcfce367af39bf6ea5c677
jklabay <jeffklein.labay@gmail.com>
2023-05-25 17:55:21 +0800

    Minor Changes

--- Frontend/src/components/AddPaymentModal.tsx
+++ Frontend/src/components/AddPaymentModal.tsx
@@ -36,10 +36,10 @@ const AddProductModal = ({ setShowModal, setReload }: any) => {
   return (
     <Modal
       onClick={() => setShowModal(false)}
-      className="flex flex-col justify-center items-center bg-white"
+      className="flex flex-col justify-center items-center bg-gradient-to-r from-slate-400 to-lime-500 antialiased leading-relaxed"
     >
-      <h1 className="font-bold text-[2rem] mb-[1.5rem] text-sky-400">
-        Add Product
+      <h1 className="font-bold text-4xl mb-6 text-white">
+        Add Payment
       </h1>
       <form onSubmit={submitHandler}>
         <div className="flex gap-8 items-center">
@@ -48,7 +48,7 @@ const AddProductModal = ({ setShowModal, setReload }: any) => {
               <div className="mb-4">
                 <label
                   htmlFor="productCode"
-                  className="font-semibold text-[1.15rem]"
+                  className="font-semibold text-[1.15rem]  text-white"
                 >
                   Product Code
                 </label>
@@ -62,7 +62,7 @@ const AddProductModal = ({ setShowModal, setReload }: any) => {
               <div className="mb-4">
                 <label
                   htmlFor="productName"
-                  className="font-semibold text-[1.15rem]"
+                  className="font-semibold text-[1.15rem]  text-white"
                 >
                   Product Name
                 </label>
@@ -76,7 +76,7 @@ const AddProductModal = ({ setShowModal, setReload }: any) => {
               <div className="block mb-4">
                 <label
                   htmlFor="productLine"
-                  className="font-semibold text-[1.15rem]"
+                  className="font-semibold text-[1.15rem]  text-white"
                 >
                   Product Line
                 </label>
@@ -90,7 +90,7 @@ const AddProductModal = ({ setShowModal, setReload }: any) => {
               <div className="mb-[1rem]">
                 <label
                   htmlFor="productScale"
-                  className="font-semibold text-[1.15rem]"
+                  className="font-semibold text-[1.15rem]  text-white"
                 >
                   Product Scale
                 </label>
@@ -105,7 +105,7 @@ const AddProductModal = ({ setShowModal, setReload }: any) => {
                 <div className="mb-[1rem]">
                   <label
                     htmlFor="productVendor"
-                    className="font-semibold text-[1.15rem]"
+                    className="font-semibold text-[1.15rem]  text-white"
                   >
                     Product Vendor
                   </label>
@@ -123,7 +123,7 @@ const AddProductModal = ({ setShowModal, setReload }: any) => {
                 <div className="mb-[1rem]">
                   <label
                     htmlFor="productDesc"
-                    className="font-semibold text-[1.15rem]"
+                    className="font-semibold text-[1.15rem]  text-white"
                   >
                     Product Description
                   </label>
@@ -139,7 +139,7 @@ const AddProductModal = ({ setShowModal, setReload }: any) => {
                 <div className="mb-[1rem]">
                   <label
                     htmlFor="quantityInStock"
-                    className="font-semibold text-[1.15rem]"
+                    className="font-semibold text-[1.15rem]  text-white"
                   >
                     Quantity In Stock
                   </label>
@@ -154,7 +154,7 @@ const AddProductModal = ({ setShowModal, setReload }: any) => {
               <div className="block mb-4">
                 <label
                   htmlFor="buyPrice"
-                  className="font-semibold text-[1.15rem]"
+                  className="font-semibold text-[1.15rem]  text-white"
                 >
                   Buy Price
                 </label>
@@ -166,7 +166,7 @@ const AddProductModal = ({ setShowModal, setReload }: any) => {
                 />
               </div>
               <div className="block mb-4">
-                <label htmlFor="MSRP" className="font-semibold text-[1.15rem]">
+                <label htmlFor="MSRP" className="font-semibold text-[1.15rem]  text-white">
                   MSRP
                 </label>
                 <input
@@ -180,7 +180,14 @@ const AddProductModal = ({ setShowModal, setReload }: any) => {
             <div className="text-center">
               <button
                 type="submit"
-                className="bg-sky-400 mt-[1.5rem] text-white h-[2.8rem] rounded w-[20rem] font-semibold transition-all duration-[0.3s] ease-in-out hover:bg-blue-600"
+                className="bg-green-500 mt-6
+                text-white h-10 
+                rounded w-72 
+                font-semibold 
+                transition-all 
+                duration-300 
+                ease-in-out
+                hover:bg-green-600"
               >
                 Add Product
               </button>
